dda3c90e341cebbc3a035ad1602e379715a4f02c
win-luck <chaso1495@naver.com>
2024-07-23 11:58:04 +0900

    [init] first commit

---
+++ src/main/java/hyundai/softeer/orange/OrangeApplication.java
@@ -0,0 +1,13 @@
+package hyundai.softeer.orange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hyundai/softeer/orange/OrangeApplicationTests.java
@@ -0,0 +1,13 @@
+package hyundai.softeer.orange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
