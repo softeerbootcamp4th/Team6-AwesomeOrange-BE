aad94081f59511e32f24c550b77a5b6abe38b229
win-luck <chaso1495@naver.com>
2024-07-31 14:35:33 +0900

    [chore] EventUser 관련 유틸 및 예외처리 추가 (#17)

--- src/main/java/hyundai/softeer/orange/common/util/MessageUtil.java
+++ src/main/java/hyundai/softeer/orange/common/util/MessageUtil.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MessageUtil {
 
-    public static final String BAD_INPUT = "잘못된 값이 입력되었거나 값이 누락되었습니다.";
+    public static final String BAD_INPUT = "null 혹은 빈 값이 입력되었습니다.";
     public static final String OUT_OF_SIZE = "정해진 크기를 벗어났습니다.";
+    public static final String INVALID_PHONE_NUMBER = "올바르지 않은 전화번호 형식입니다.";
+    public static final String INVALID_AUTH_CODE = "인증번호는 숫자로만 입력해주세요.";
 }
