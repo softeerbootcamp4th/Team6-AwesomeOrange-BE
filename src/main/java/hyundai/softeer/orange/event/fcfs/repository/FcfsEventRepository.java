e275c8881b807f3b5fb0940c03eebd170a18022b
win-luck <chaso1495@naver.com>
2024-08-01 14:31:31 +0900

    [feat] 선착순 이벤트 관련 Repository 메서드 추가 (#23)

--- src/main/java/hyundai/softeer/orange/event/fcfs/repository/FcfsEventRepository.java
+++ src/main/java/hyundai/softeer/orange/event/fcfs/repository/FcfsEventRepository.java
@@ -4,6 +4,11 @@ import hyundai.softeer.orange.event.fcfs.entity.FcfsEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface FcfsEventRepository extends JpaRepository<FcfsEvent, Long> {
+
+    List<FcfsEvent> findByStartTimeBetween(LocalDateTime startTime, LocalDateTime endTime);
 }
--- src/main/java/hyundai/softeer/orange/event/fcfs/repository/FcfsEventWinningInfoRepository.java
+++ src/main/java/hyundai/softeer/orange/event/fcfs/repository/FcfsEventWinningInfoRepository.java
@@ -2,8 +2,15 @@ package hyundai.softeer.orange.event.fcfs.repository;
 
 import hyundai.softeer.orange.event.fcfs.entity.FcfsEventWinningInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FcfsEventWinningInfoRepository extends JpaRepository<FcfsEventWinningInfo, Long> {
+
+    // Fetch Join으로 eventUser 정보까지 한번에 가져와서 N+1 문제 방지
+    @Query("select f from FcfsEventWinningInfo f join fetch f.eventUser where f.fcfsEvent.id = :eventSequence")
+    List<FcfsEventWinningInfo> findByFcfsEventId(Long eventSequence);
 }
