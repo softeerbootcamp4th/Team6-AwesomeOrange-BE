f51aab5246f7b3ec4670ef0abf3a7d3fa39a062b
win-luck <chaso1495@naver.com>
2024-08-01 14:31:47 +0900

    [feat] FcfsEventWinningInfo 생성메서드 추가 (#23)

--- src/main/java/hyundai/softeer/orange/event/fcfs/entity/FcfsEventWinningInfo.java
+++ src/main/java/hyundai/softeer/orange/event/fcfs/entity/FcfsEventWinningInfo.java
@@ -2,10 +2,13 @@ package hyundai.softeer.orange.event.fcfs.entity;
 
 import hyundai.softeer.orange.eventuser.entity.EventUser;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name="fcfs_event_winning_info")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class FcfsEventWinningInfo {
     @Id
@@ -19,4 +22,11 @@ public class FcfsEventWinningInfo {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name ="event_user_id")
     private EventUser eventUser;
+
+    public static FcfsEventWinningInfo of(FcfsEvent fcfsEvent, EventUser eventUser) {
+        FcfsEventWinningInfo fcfsEventWinningInfo = new FcfsEventWinningInfo();
+        fcfsEventWinningInfo.fcfsEvent = fcfsEvent;
+        fcfsEventWinningInfo.eventUser = eventUser;
+        return fcfsEventWinningInfo;
+    }
 }
