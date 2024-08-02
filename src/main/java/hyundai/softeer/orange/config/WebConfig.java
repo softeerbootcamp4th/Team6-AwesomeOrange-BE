f7150fe30661775f1aebfa9f4135e8726413aff5
win-luck <chaso1495@naver.com>
2024-08-02 10:09:18 +0900

    [fix] Swagger 진입 불가능하던 버그 수정 (#26)

--- src/main/java/hyundai/softeer/orange/config/WebConfig.java
+++ src/main/java/hyundai/softeer/orange/config/WebConfig.java
@@ -20,7 +20,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authInterceptor);
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/swagger-ui/**");
     }
 
     @Override
