69bfb1304415ca1111814099fab95e7e1e0377ce
blaxsior <heejun2798@gmail.com>
2024-08-05 17:30:34 +0900

    [fix] LocalDateTime이 클라이언트에 문자열로 파싱되도록 수정

--- src/main/java/hyundai/softeer/orange/config/AppConfig.java
+++ src/main/java/hyundai/softeer/orange/config/AppConfig.java
@@ -1,6 +1,7 @@
 package hyundai.softeer.orange.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,8 @@ public class AppConfig {
         ObjectMapper objectMapper = new ObjectMapper();
         // LocalDateTime파싱이 목적
         objectMapper.registerModule(new JavaTimeModule());
+        // timestamp를 문자열로 전달
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
     }
 }
