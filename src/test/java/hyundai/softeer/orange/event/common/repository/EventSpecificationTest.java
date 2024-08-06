2a49e58e811b9b814f7b226baebfd6aa7a25bd4a
blaxsior <heejun2798@gmail.com>
2024-08-06 15:56:51 +0900

    [test] event spec search에 대한 테스트 추가(#32)

--- src/test/java/hyundai/softeer/orange/event/common/repository/EventSpecificationTest.java
+++ src/test/java/hyundai/softeer/orange/event/common/repository/EventSpecificationTest.java
@@ -2,24 +2,17 @@ package hyundai.softeer.orange.event.common.repository;
 
 import hyundai.softeer.orange.event.common.entity.EventFrame;
 import hyundai.softeer.orange.event.common.entity.EventMetadata;
-import net.bytebuddy.description.annotation.AnnotationValue;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.TestPropertySource;
 
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 @TestPropertySource(locations = "classpath:application-test.yml")
 class EventSpecificationTest {
