f63b145c58d18a70bd3487b26b532fff32db8d35
win-luck <chaso1495@naver.com>
2024-08-06 15:48:50 +0900

    [chore] swagger controller 반환값 구체화 (#33)

--- src/main/java/hyundai/softeer/orange/comment/controller/CommentController.java
+++ src/main/java/hyundai/softeer/orange/comment/controller/CommentController.java
@@ -4,7 +4,10 @@ import hyundai.softeer.orange.comment.dto.CreateCommentDto;
 import hyundai.softeer.orange.comment.dto.ResponseCommentsDto;
 import hyundai.softeer.orange.comment.service.ApiService;
 import hyundai.softeer.orange.comment.service.CommentService;
+import hyundai.softeer.orange.common.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -25,7 +28,10 @@ public class CommentController {
 
     @Tag(name = "Comment")
     @GetMapping
-    @Operation(summary = "기대평 조회", description = "주기적으로 추출되는 긍정 기대평 목록을 조회한다.")
+    @Operation(summary = "기대평 조회", description = "주기적으로 추출되는 긍정 기대평 목록을 조회한다.", responses = {
+            @ApiResponse(responseCode = "200", description = "기대평 조회 성공",
+                    content = @Content(schema = @Schema(implementation = ResponseCommentsDto.class)))
+    })
     public ResponseEntity<ResponseCommentsDto> getComments() {
         return ResponseEntity.ok(commentService.getComments());
     }
@@ -33,9 +39,12 @@ public class CommentController {
     @Tag(name = "Comment")
     @PostMapping
     @Operation(summary = "기대평 등록", description = "유저가 신규 기대평을 등록한다.", responses = {
-            @ApiResponse(responseCode = "200", description = "기대평 등록 성공"),
-            @ApiResponse(responseCode = "400", description = "기대평 등록 실패, 지나치게 부정적인 표현으로 간주될 때"),
-            @ApiResponse(responseCode = "409", description = "하루에 여러 번의 기대평을 작성하려 할 때")
+            @ApiResponse(responseCode = "200", description = "기대평 등록 성공",
+                    content = @Content(schema = @Schema(implementation = Boolean.class))),
+            @ApiResponse(responseCode = "400", description = "기대평 등록 실패, 지나치게 부정적인 표현으로 간주될 때",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "409", description = "하루에 여러 번의 기대평을 작성하려 할 때",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     public ResponseEntity<Boolean> createComment(@RequestBody @Valid CreateCommentDto dto) {
         boolean isPositive = apiService.analyzeComment(dto.getContent());
--- src/main/java/hyundai/softeer/orange/event/url/controller/UrlController.java
+++ src/main/java/hyundai/softeer/orange/event/url/controller/UrlController.java
@@ -25,11 +25,11 @@ public class UrlController {
     @PostMapping("/shorten")
     @Operation(summary = "URL 단축", description = "URL을 단축하여 반환합니다.", responses = {
             @ApiResponse(responseCode = "200", description = "URL 단축 성공",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseUrlDto.class))),
+                    content = @Content(schema = @Schema(implementation = ResponseUrlDto.class))),
             @ApiResponse(responseCode = "400", description = "URL 형식이 잘못되었을 때",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
             @ApiResponse(responseCode = "404", description = "유저를 찾지 못했을 때",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     public ResponseEntity<ResponseUrlDto> urlShorten(@RequestParam String originalUrl, @RequestParam String userId){
         // TODO: JWT 토큰으로부터 userId를 추출하여 사용하도록 추후 수정
@@ -41,7 +41,7 @@ public class UrlController {
     @Operation(summary = "URL 리다이렉트", description = "단축 URL을 원본 URL로 리다이렉트합니다.", responses = {
             @ApiResponse(responseCode = "302", description = "URL 리다이렉트"),
             @ApiResponse(responseCode = "404", description = "단축 URL을 찾지 못했을 때",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     public ResponseEntity<Void> redirectToOriginalUrl(@PathVariable String shortUrl){
         return ResponseEntity.status(302)
