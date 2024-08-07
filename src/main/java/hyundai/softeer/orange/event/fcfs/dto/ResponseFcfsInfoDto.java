70bd40b754bd736025cb19570ab9bf7a8e341062
win-luck <chaso1495@naver.com>
2024-08-07 17:49:39 +0900

    [build] 불필요한 것으로 판정된 jackson 라이브러리 삭제 및 관련 코드 제거 (#35)

--- src/main/java/hyundai/softeer/orange/event/fcfs/dto/ResponseFcfsInfoDto.java
+++ src/main/java/hyundai/softeer/orange/event/fcfs/dto/ResponseFcfsInfoDto.java
@@ -1,22 +1,16 @@
 package hyundai.softeer.orange.event.fcfs.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ResponseFcfsInfoDto implements Serializable {
+public class ResponseFcfsInfoDto {
 
-    @JsonSerialize(using = LocalDateTimeSerializer.class) // 직렬화 시 필요
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
     private LocalDateTime nowDateTime;
 
     private String eventStatus;
