b1fc0186f620ffd417a62ae1ca4b71cc3e7f4e57
win-luck <chaso1495@naver.com>
2024-08-07 11:32:23 +0900

    [chore] WebConfig resolver 반영 및 생성자 주입으로 변경 (#35)

--- src/main/java/hyundai/softeer/orange/config/WebConfig.java
+++ src/main/java/hyundai/softeer/orange/config/WebConfig.java
@@ -2,6 +2,8 @@ package hyundai.softeer.orange.config;
 
 import hyundai.softeer.orange.admin.component.AdminArgumentResolver;
 import hyundai.softeer.orange.core.auth.AuthInterceptor;
+import hyundai.softeer.orange.eventuser.component.EventUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -10,13 +12,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Autowired
-    private AuthInterceptor authInterceptor;
 
-    @Autowired
-    private AdminArgumentResolver adminArgumentResolver;
+    private final AuthInterceptor authInterceptor;
+    private final AdminArgumentResolver adminArgumentResolver;
+    private final EventUserArgumentResolver eventUserArgumentResolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -28,5 +30,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(adminArgumentResolver);
+        resolvers.add(eventUserArgumentResolver);
     }
 }
