7c50d87b7e69892ea00d410a0469f5dfe227a1a5
win-luck <chaso1495@naver.com>
2024-08-07 14:04:58 +0900

    [refactor] FcfsController ArgumentResolver 적용 (#35)

--- src/main/java/hyundai/softeer/orange/event/fcfs/controller/FcfsController.java
+++ src/main/java/hyundai/softeer/orange/event/fcfs/controller/FcfsController.java
@@ -1,9 +1,13 @@
 package hyundai.softeer.orange.event.fcfs.controller;
 
 import hyundai.softeer.orange.common.ErrorResponse;
+import hyundai.softeer.orange.core.auth.Auth;
+import hyundai.softeer.orange.core.auth.AuthRole;
 import hyundai.softeer.orange.event.fcfs.dto.ResponseFcfsResultDto;
 import hyundai.softeer.orange.event.fcfs.service.FcfsAnswerService;
 import hyundai.softeer.orange.event.fcfs.service.FcfsService;
+import hyundai.softeer.orange.eventuser.component.EventUserAnnotation;
+import hyundai.softeer.orange.eventuser.dto.EventUserInfo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -22,6 +26,7 @@ public class FcfsController {
     private final FcfsService fcfsService;
     private final FcfsAnswerService fcfsAnswerService;
 
+    @Auth(AuthRole.event_user)
     @Tag(name = "fcfs")
     @PostMapping
     @Operation(summary = "선착순 이벤트 참여", description = "선착순 이벤트에 참여한 결과(boolean)를 반환한다.", responses = {
@@ -30,9 +35,9 @@ public class FcfsController {
             @ApiResponse(responseCode = "400", description = "선착순 이벤트 시간이 아니거나, 요청 형식이 잘못된 경우",
                     content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
-    public ResponseEntity<ResponseFcfsResultDto> participate(@RequestParam Long eventSequence, @RequestParam String userId, @RequestParam String eventAnswer) {
+    public ResponseEntity<ResponseFcfsResultDto> participate(@EventUserAnnotation EventUserInfo userInfo, @RequestParam Long eventSequence, @RequestParam String eventAnswer) {
         boolean answerResult = fcfsAnswerService.judgeAnswer(eventSequence, eventAnswer);
-        boolean isWin = answerResult && fcfsService.participate(eventSequence, userId);
+        boolean isWin = answerResult && fcfsService.participate(eventSequence, userInfo.getUserId());
         return ResponseEntity.ok(new ResponseFcfsResultDto(answerResult, isWin));
     }
 }
